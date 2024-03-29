a77ab47d232ddc3d14856a036f31460dc61ef351
Danil <danil.r.savchenko@gmail.com>
2024-03-29 16:22:43 +0200

    Lesson 26 Spring Boot Starter

--- src/main/java/by/danilko/spring/config/ApplicationConfiguration.java
+++ src/main/java/by/danilko/spring/config/ApplicationConfiguration.java
@@ -1,11 +1,9 @@
 package by.danilko.spring.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
-@ComponentScan("by.danilko.spring")
+
 public class ApplicationConfiguration {
 }
