ddcb053b9e014bb8554c11653e06f1a119a50760
Danil <danil.r.savchenko@gmail.com>
2024-03-30 00:50:39 +0200

    lesson 27 spring properties

--- src/main/java/by/danilko/spring/ApplicationRunner.java
+++ src/main/java/by/danilko/spring/ApplicationRunner.java
@@ -1,12 +1,16 @@
 package by.danilko.spring;
 
 
+import by.danilko.spring.config.DatabaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApplicationRunner {
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationRunner.class, args);
+        var context = SpringApplication.run(ApplicationRunner.class, args);
+        System.out.println(context.getBean(DatabaseProperties.class));
     }
 }
