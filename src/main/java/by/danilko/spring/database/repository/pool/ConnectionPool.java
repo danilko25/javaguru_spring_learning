4e0f07893951dd72074c0154156b343fac5c9ff8
Danil <danil.r.savchenko@gmail.com>
2024-03-30 21:25:43 +0200

    lesson 28 spring logging

--- src/main/java/by/danilko/spring/database/repository/pool/ConnectionPool.java
+++ src/main/java/by/danilko/spring/database/repository/pool/ConnectionPool.java
@@ -1,9 +1,12 @@
 package by.danilko.spring.database.repository.pool;
 
+import jakarta.annotation.PostConstruct;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @ToString
 public class ConnectionPool {
@@ -20,5 +23,10 @@ public class ConnectionPool {
         this.password = password;
         this.poolSize = poolSize;
         this.url = url;
+    }
+
+    @PostConstruct
+    public void init(){
+        log.info("Init connection pool");
     }
 }
